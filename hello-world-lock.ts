913a087bcd3b6b690f583dfae79593f53d8b5ea0
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-03-29 13:04:58 +0100

    Creating script to lock funds into smart contract. issue with Blockfrost await resolution

---
+++ hello-world-lock.ts
@@ -0,0 +1,21 @@
+import {
+    Blockfrost,
+    C,
+    Constr,
+    Data,
+    Lucid,
+    SpendingValidator,
+    TxHash,
+    fromHex,
+    toHex,
+    utf8ToHex,
+  } from "https://deno.land/x/lucid@0.8.3/mod.ts";
+  import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
+   
+  const lucid = await Lucid.new(
+    new Blockfrost(
+      "https://cardano-preview.blockfrost.io/api/v0",
+      Deno.env.get("BLOCKFROST_PROJECT_ID")
+    ),
+    "Preview"
+  );
